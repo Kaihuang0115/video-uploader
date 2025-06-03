4fdb6a4c4ee081e3de9c299796dd8fae6dc6f14e
Kaihuang0115 <roget0115@gmail.com>
2025-06-03 16:08:56 +0800

    Resolve merge conflict in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-// tailwind.config.js
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-=======
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
->>>>>>> 288b56b (加入 Auth 判斷，只有管理員可上傳/編輯/刪除影片)
   theme: {
     extend: {},
   },
